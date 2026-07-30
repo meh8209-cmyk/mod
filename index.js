f48c8ca660bfd8abec3183e8b6c0c465ca5ed8d5
meh8209-cmyk <meh8209@gmail.com>
2026-07-30 15:27:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = process.env.bot_token
-const id = process.env.bot_id
-const address = 'https://www.google.com'
+const token = '8998240783:AAFy4gEyliFZUeFd4NreC8sHafoe8pUdExg'
+const id = '7863774802'
+const address = 'https://www.youtube/.com'
 
 const app = express();
 const appServer = http.createServer(app);
